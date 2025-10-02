7f96b260942394a161f7d08de140bb41b0770bc7
trose2012 <theo.c.rose@gmail.com>
2025-10-02 11:29:23 -0400

    buta buh bun aurggb

--- player.gd
+++ player.gd
@@ -12,6 +12,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("Restart"):
+		get_tree().reload_current_scene()
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
